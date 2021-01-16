0d9278e956dbab6a31166f662a6f5b669b6d2106
eduardamirelly <eduardamirelly404@outlook.com>
2021-01-16 18:44:28 -0300

    Efeito Parallax no background da Fase-01

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothSpeed = 0.08f;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        Vector3 startPosition = new Vector3(target.position.x, transform.position.y, -1f);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, startPosition, smoothSpeed);
+        transform.position = smoothPosition;
+    }
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    private float lengthBackground;
+    private float startPosition;
+    private Transform cam;
+    public float ParallaxEffect;
+
+    void Start(){
+        startPosition = transform.position.x;
+        lengthBackground = GetComponent<SpriteRenderer>().bounds.size.x;
+        cam = Camera.main.transform;
+    }
+
+    void FixedUpdate(){
+        float rePosition = cam.transform.position.x * (1 - ParallaxEffect);
+        float Distance = cam.transform.position.x * ParallaxEffect;
+        transform.position = new Vector3(startPosition + Distance, transform.position.y, transform.position.z);
+
+        if(rePosition > startPosition-lengthBackground/2){
+            startPosition += lengthBackground;
+        }else if(rePosition < startPosition+lengthBackground/2){
+            startPosition -= lengthBackground;
+        }
+    }
+
+}
