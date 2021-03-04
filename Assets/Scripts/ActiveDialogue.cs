0cc4b3002a2eb02926b9ee0babcac30083b1d848
eduardamirelly <eduardamirelly404@outlook.com>
2021-03-04 20:17:09 -0300

    Em produção - Diálogos

---
+++ Assets/Scripts/ActiveDialogue.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveDialogue : MonoBehaviour
+{
+    
+    public string speechText;
+    public string actorName;
+
+    public LayerMask playerLayer;
+    public float radious;
+
+    private DialogueControl dc;
+
+    private void Start() {
+        dc = FindObjectOfType<DialogueControl>();
+    }
+
+    private void FixedUpdate(){
+        ShowDialogue();
+    }
+
+    public void ShowDialogue(){
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radious, playerLayer);
+        if(hit != null){
+            dc.Speech(speechText, actorName);
+        }else{
+
+        }
+
+    }
+
+}
