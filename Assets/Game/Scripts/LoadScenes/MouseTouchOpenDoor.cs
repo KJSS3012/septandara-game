146b90691d529228b5e234edeb9dfff025df697a
eduardamirelly <eduardamirelly404@outlook.com>
2021-06-18 15:12:12 -0300

    Mudança de cenas

---
+++ Assets/Game/Scripts/LoadScenes/MouseTouchOpenDoor.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MouseTouchOpenDoor : MonoBehaviour
+{
+
+    public GameObject fadeUI;
+    public Animator animFade;
+    [SerializeField] private Animator animObject;
+    public int sceneIndex;
+    public bool isMouseDown;
+    public bool isCollider;
+
+
+    private void OnMouseDown()
+    {
+        isMouseDown = true;
+    }
+
+    private void OnMouseExit()
+    {
+        isMouseDown = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isCollider = true;   
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isCollider = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isMouseDown && isCollider)
+        {
+            animObject.SetBool("open", true);
+        }
+    }
+
+    public void StartTransition()
+    {
+        fadeUI.SetActive(true);
+        animFade.SetTrigger("out");
+    }
+
+  
+}
