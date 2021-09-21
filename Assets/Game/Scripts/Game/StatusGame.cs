e0e61fc31e6ebe4db7cc2e535333efce34fd444e
eduardamirelly <mirelly.eduarda404@gmail.com>
2021-09-21 18:36:44 -0300

    Mudança de expressão facial dependendo da porcentagem de vida da personagem

--- Assets/Game/Scripts/Game/StatusGame.cs
+++ Assets/Game/Scripts/Game/StatusGame.cs
@@ -7,6 +7,7 @@ public class StatusGame : ScriptableObject
 {
 
     public bool[] chances; //chances ativas da personagem
+    public int consecutiveQuestion;
     public int quantLife; //porcentagem de vida
     public int scoreCoins; //quantidade de moedas
 
